03c56ccd423b709e01c15e1488e14abc177f857e
qweasdzxcll <denispodmogilnyj@gmail.com>
2025-01-23 19:05:37 +0300

    Развертывание проекта на github

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,12 +1,34 @@
-import { getCLS, getFID, getFCP, getLCP, getTTFB } from 'web-vitals';
-
 const reportWebVitals = (onPerfEntry) => {
-  if (onPerfEntry && typeof onPerfEntry === 'function') {
-    getCLS(onPerfEntry);
-    getFID(onPerfEntry);
-    getFCP(onPerfEntry);
-    getLCP(onPerfEntry);
-    getTTFB(onPerfEntry);
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals')
+      .then(({ getFID, getFCP, getLCP, getTTFB }) => {
+        if (getFID) {
+          getFID(onPerfEntry);
+        } else {
+          console.error('getFID is not defined');
+        }
+
+        if (getFCP) {
+          getFCP(onPerfEntry);
+        } else {
+          console.error('getFCP is not defined');
+        }
+
+        if (getLCP) {
+          getLCP(onPerfEntry);
+        } else {
+          console.error('getLCP is not defined');
+        }
+
+        if (getTTFB) {
+          getTTFB(onPerfEntry);
+        } else {
+          console.error('getTTFB is not defined');
+        }
+      })
+      .catch((error) => {
+        console.error('Ошибка при импорте web-vitals:', error);
+      });
   }
 };
 
